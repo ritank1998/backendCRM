b237ee42b64f69271001ef8ea01435412b414d02
RitankS <146180073+RitankS@users.noreply.github.com>
2024-05-15 20:42:44 +0530

    connection updates

--- mongodb/conn.js
+++ mongodb/conn.js
@@ -1,7 +1,13 @@
 import mongoose from "mongoose"
-mongoose.connect("mongodb+srv://rihinatechorzo:Ritank%401998@cluster1.281wep1.mongodb.net/?retryWrites=true&w=majority&appName=cluster1",{
-    useNewUrlParser : true,
-    useUnifiedTopology : true
-}).then(()=>{
-    console.log("Connection Established")
-}).catch((e)=>console.log("Connection Not Established...."))
+mongoose.connect("mongodb+srv://rihinatechorzo:Ritank%401998@cluster1.281wep1.mongodb.net/?retryWrites=true&w=majority&appName=cluster1")
+    .then(() => {
+        console.log('MongoDB Connnected...')
+    }).catch((err) => {
+        console.log('Error while Mongo Conn..', err);
+    })
+// mongoose.connect("mongodb+srv://rihinatechorzo:Ritank%401998@cluster1.281wep1.mongodb.net/?retryWrites=true&w=majority&appName=cluster1",{
+//     useNewUrlParser : true,
+//     useUnifiedTopology : true
+// }).then(()=>{
+//     console.log("Connection Established")
+// }).catch((e)=>console.log("Connection Not Established...."))
